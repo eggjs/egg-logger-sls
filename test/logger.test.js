868938b758b491e540b2156623e4d564e65c0bbe
popomore <sakura9515@gmail.com>
2018-08-26 11:54:15 +0800

    f

--- test/logger.test.js
+++ test/logger.test.js
@@ -8,47 +8,95 @@ const sleep = require('mz-modules/sleep');
 const hostname = os.hostname();
 
 describe('test/logger.test.js', () => {
-  let app;
-  before(() => {
-    app = mock.app({
-      baseDir: 'apps/logger',
-    });
-    return app.ready();
-  });
-
-  after(() => app.close());
   afterEach(mock.restore);
 
-  it('should GET /', async () => {
-    let logs = [];
-    mock(app.sls, 'postLogstoreLogs', async (g, l, group) => {
-      assert(group.source === hostname);
-      logs = group.logs.concat(logs);
+  describe('sls client', () => {
+    let app;
+    before(() => {
+      app = mock.app({
+        baseDir: 'apps/client',
+      });
+      return app.ready();
     });
-    await app.httpRequest()
-      .get('/')
-      .expect('done')
-      .expect(200);
+    after(() => app.close());
 
-    await sleep(2000);
+    it('should upload success', async () => {
+      let logs = [];
+      mock(app.sls, 'postLogstoreLogs', async (g, l, group) => {
+        assert(group.source === hostname);
+        logs = group.logs.concat(logs);
+      });
+      await app.httpRequest()
+        .get('/')
+        .expect('done')
+        .expect(200);
 
-    for (const log of logs) {
-      assert(typeof log.time === 'number');
-      assert(String(log.time).length === 10);
+      await sleep(2000);
 
-      const keys = [];
-      for (const { key } of log.contents) {
-        keys.push(key);
+      for (const log of logs) {
+        assert(typeof log.time === 'number');
+        assert(String(log.time).length === 10);
+
+        const keys = [];
+        for (const { key } of log.contents) {
+          keys.push(key);
+        }
+        assert.deepEqual(keys, [
+          'level',
+          'content',
+          'ip',
+          'hostname',
+          'appName',
+          'loggerName',
+          'loggerFileName',
+        ]);
       }
-      assert.deepEqual(keys, [
-        'level',
-        'content',
-        'ip',
-        'hostname',
-        'appName',
-        'loggerName',
-        'loggerFileName',
-      ]);
-    }
+    });
   });
+
+  describe('sls clients', () => {
+
+    let app;
+    before(() => {
+      app = mock.app({
+        baseDir: 'apps/clients',
+      });
+      return app.ready();
+    });
+    after(() => app.close());
+
+    it('should upload success', async () => {
+      let logs = [];
+      mock(app.sls, 'postLogstoreLogs', async (g, l, group) => {
+        assert(group.source === hostname);
+        logs = group.logs.concat(logs);
+      });
+      await app.httpRequest()
+        .get('/')
+        .expect('done')
+        .expect(200);
+
+      await sleep(2000);
+
+      for (const log of logs) {
+        assert(typeof log.time === 'number');
+        assert(String(log.time).length === 10);
+
+        const keys = [];
+        for (const { key } of log.contents) {
+          keys.push(key);
+        }
+        assert.deepEqual(keys, [
+          'level',
+          'content',
+          'ip',
+          'hostname',
+          'appName',
+          'loggerName',
+          'loggerFileName',
+        ]);
+      }
+    });
+  });
+
 });
