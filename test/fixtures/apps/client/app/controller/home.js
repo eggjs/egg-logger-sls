125609682871c6def6c4aca5fb6d4ce132e0e1a7
Haoliang Gao <sakura9515@gmail.com>
2018-08-26 13:08:04 +0800

    feat: logger can be disabled (#4)

--- test/fixtures/apps/client/app/controller/home.js
+++ test/fixtures/apps/client/app/controller/home.js
@@ -10,6 +10,11 @@ class HomeController extends Controller {
     this.ctx.getLogger('myLogger').info('my logger');
     this.ctx.body = 'done';
   }
+
+  async disable() {
+    this.ctx.getLogger('disabledLogger').info('disabled logger');
+    this.ctx.body = 'done';
+  }
 }
 
 module.exports = HomeController;
