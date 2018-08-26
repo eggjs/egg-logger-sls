867248adb6377ae20abea5f583b819d551bb31fd
Haoliang Gao <sakura9515@gmail.com>
2018-08-26 13:08:04 +0800

    feat: logger can be disabled (#4)

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const SLSLoggerClient = require('./lib/sls_logger_client');
 const SLSTransport = require('./lib/sls_transport');
 
 module.exports = app => {
+  const config = app.config.loggerSLS;
   const client = new SLSLoggerClient(app);
 
   for (const [ name, logger ] of app.loggers.entries()) {
+    if (config.disable.includes(name)) continue;
+
     const transport = new SLSTransport({
       level: 'DEBUG',
       consoleLevel: 'NONE',
