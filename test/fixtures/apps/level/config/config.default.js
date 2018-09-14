af5213ecd1f33a650fdabc7d302cc39d11639437
Haoliang Gao <sakura9515@gmail.com>
2018-09-15 00:35:01 +0800

    fix: the level of transport should extend from logger (#9)

---
+++ test/fixtures/apps/level/config/config.default.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.keys = '123456';
+
+exports.sls = {
+  client: {
+    endpoint: process.env.SLS_ENDPOINT,
+    accessKeyId: process.env.SLS_ACCESS_KEY_ID,
+    accessKeySecret: process.env.SLS_ACCESS_KEY_SECRET,
+  },
+};
+
+exports.loggerSLS = {
+  project: 'egg-sls-post-log',
+  logstore: 'egg-sls-post-log',
+};
+
+exports.logger = {
+  level: 'WARN',
+};
---
+++ test/fixtures/apps/level/config/plugin.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  sls: {
+    enable: true,
+    package: 'egg-sls',
+  },
+};
