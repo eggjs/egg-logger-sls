835bd3e2dba13ec5421215d3b5930d929a7f13c1
killa <killa123@126.com>
2019-06-10 14:29:46 +0800

    fix: split logs when PostBodyTooLarge (#16)

--- test/fixtures/apps/client/app/router.js
+++ test/fixtures/apps/client/app/router.js
@@ -4,6 +4,8 @@ module.exports = app => {
   const { router, controller } = app;
 
   router.get('/', controller.home.index);
+  router.get('/bigLog', controller.home.bigLog);
+  router.get('/singleBigLog', controller.home.singleBigLog);
   router.get('/disable', controller.home.disable);
   router.get('/transform', controller.home.transform);
 };
